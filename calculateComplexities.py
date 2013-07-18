e18c06f30d6c34c7f2c57b084f09aced6f79a4d4
jcgood <jcgood@buffalo.edu>
2013-07-18 13:46:21 -0400

    More improvements to complexity calculations.

--- calculateComplexities.py
+++ calculateComplexities.py
@@ -2,28 +2,127 @@
 
 # A native python MySQL library
 import pymysql
+import rpy2.robjects as robj
 
-#conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='APiCS')
-conn = pymysql.connect(host='localhost', port=3307, user='root', passwd='', db='APiCS')
+conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='', db='APiCS')
+#conn = pymysql.connect(host='localhost', port=3307, user='root', passwd='', db='APiCS')
 cur = conn.cursor()
 
 
 cur.execute("SELECT `Feature_number`, `Values_number`, Complexity FROM WALSValueInfo")
-
 complexities = { }
 for row in cur.fetchall():
-	feat, value, complexity = row
-	
+	feat, value, complexity = row	
 	if feat in complexities:
 		compfeat = complexities[feat]
 		compfeat[str(value)] = complexity
-		complexities[feat] = compfeat
-		
+		complexities[feat] = compfeat		
 	else:
 		compfeat = {}
 		compfeat[str(value)] = complexity
 		complexities[feat] = compfeat
 
+
+
+# Make a dictionary for feature names
+cur.execute("SELECT `Feature_number`, `Feature_name` FROM WALSFeatureNames")
+names = { }
+for row in cur.fetchall():
+	feat, name = row	
+	names[feat] = name
+
+
+# Make a dictionary for feature types
+cur.execute("SELECT `WALS-APICS`, `ComplexityType` FROM APiCSFeatures")
+types = { }
+for row in cur.fetchall():
+	feat, type = row	
+	if feat != "None":
+		types[feat] = type
+
+
+
+# Go through the WALS-like APiCS values and (i) calculate complexity averages and (ii) build up lists of values for statistical processing
+cur.execute("""SELECT WALSAPiCSValues.Language, APiCSFeatures.`WALS-APICS`,  WALSAPiCSValues.Wals_value_number
+FROM WALSAPiCSValues
+INNER JOIN APiCSFeatures ON WALSAPiCSValues.`APiCS_number` = APiCSFeatures.Feature_number
+WHERE APiCSFeatures.`WALS-APICS` != \"None\"""")
+
+apicswalsFeatComp = { }
+apicswalsFeatLangCount = { }
+apicsCompList = { } # For statistical processing
+for row in cur.fetchall():
+	lang, feat, value = row
+	compfeat = complexities[feat]
+	compValue = compfeat[str(value)]
+	# Since the DB is incomplete need conditional, remove when believed complete for error checking
+	if compValue != None:
+		if feat in apicswalsFeatComp:
+			apicswalsFeatComp[feat] += compValue
+			apicswalsFeatLangCount[feat] += 1
+			apicsCompList[feat].append( compValue )
+		else:
+			apicswalsFeatComp[feat] = compValue
+			apicswalsFeatLangCount[feat] = 1
+			apicsCompList[feat] = [ compValue ]
+
+
+# Now do the same thing for the WALS languages
+cur.execute("""SELECT WALSValues.langid, WALSValues.Value_number, WALSValues.Feature_number
+FROM WALSValues""")
+
+walsFeatComp = {}
+walsFeatLangCount = { }
+walsCompList = { } # See comments above
+for row in cur.fetchall():
+	lang, value, feat = row
+	compfeat = complexities[feat]
+	compValue = compfeat[str(value)]
+	# Since the DB is incomplete need conditional
+	if compValue != None:
+		if feat in walsFeatComp:
+			walsFeatComp[feat] += compValue
+			walsFeatLangCount[feat] += 1
+			walsCompList[feat].append( compValue )
+
+		else:
+			walsFeatComp[feat] = compValue
+			walsFeatLangCount[feat] = 1
+			walsCompList[feat] = [ compValue ]
+
+#print walsCompList
+
+
+# Get average complexity for APiCS and WALS; features should be precisely the same
+for feat in walsFeatComp:
+	print feat+",", names[feat]+",", types[feat]
+	walscompval = walsFeatComp[feat]
+	walscompavg = walscompval / float(walsFeatLangCount[feat])
+	apicscompval = apicswalsFeatComp[feat]
+	apicscompavg = apicscompval / float(apicswalsFeatLangCount[feat])
+	
+	apicsvals = apicsCompList[feat]
+	walsvals = walsCompList[feat]
+	
+	apicsvector = robj.IntVector(apicsvals)
+	walsvector = robj.IntVector(walsvals)
+	t = robj.r['t.test']
+	p = t(apicsvector,walsvector,**{'var.equal': True})[2][0] # Do two-sample t-test assuming equal variance
+	
+	print "WALS average: ", walscompavg
+	print "APiCS average: ", apicscompavg
+	print "p-value = ", p
+	print ""
+
+
+cur.close()
+conn.close()
+
+## TO SELF: SEGMENTAL FEATURES?
+## TO SELF: Prohibitive is paradigmatic and syntagmatic complexity? Can I even really test syntagmatic here?
+
+
+# THIS WAS FOR WHEN I THOUGHT I COULD USE THE VARIABLE FEATURES OF APICS, BUT I HAVE NO GOOD WAY OF COMPARING THEM TO WALS
 #for f in complexities:
 #	print f, complexities[f]
 
@@ -84,82 +183,3 @@ for row in cur.fetchall():
 # 	compavg = compval / float(creoleFeatLangCount[feat])
 # 	print "APiCS: ", feat, compavg
 	
-
-# For WALS-like APiCS languages
-cur.execute("""SELECT WALSAPiCSValues.Language, APiCSFeatures.`WALS-APICS`,  WALSAPiCSValues.Wals_value_number
-FROM WALSAPiCSValues
-INNER JOIN APiCSFeatures ON WALSAPiCSValues.`APiCS_number` = APiCSFeatures.Feature_number
-WHERE APiCSFeatures.`WALS-APICS` != \"None\"""")
-
-apicswalsFeatComp = { }
-apicswalsFeatLangCount = { }
-apicsCompList = { } # For t-tests
-for row in cur.fetchall():
-	lang, feat, value = row
-	compfeat = complexities[feat]
-	compValue = compfeat[str(value)]
-	#print lang, feat, value, compValue
-	# Since the DB is incomplete need conditional
-	if compValue != None:
-		if feat in apicswalsFeatComp:
-			apicswalsFeatComp[feat] += compValue
-			apicswalsFeatLangCount[feat] += 1
-			apicsCompList[feat].append( compValue )
-		else:
-			apicswalsFeatComp[feat] = compValue
-			apicswalsFeatLangCount[feat] = 1
-			apicsCompList[feat] = [ compValue ]
-
-
-#print "xxx"
-#print apicsCompList
-
-
-# Get average complexity
-for feat in apicswalsFeatComp:
-	compval = apicswalsFeatComp[feat]
-	compavg = compval / float(apicswalsFeatLangCount[feat])
-	#print feat, compval
-	print "APiCS: ", feat, compavg
-
-
-
-# Now do the same thing for the WALS languages
-cur.execute("""SELECT WALSValues.langid, WALSValues.Value_number, WALSValues.Feature_number
-FROM WALSValues""")
-
-walsFeatComp = {}
-walsFeatLangCount = { }
-walsCompList = { } # See comments above
-for row in cur.fetchall():
-	lang, value, feat = row
-	compfeat = complexities[feat]
-	compValue = compfeat[str(value)]
-	# Since the DB is incomplete need conditional
-	if compValue != None:
-		if feat in walsFeatComp:
-			walsFeatComp[feat] += compValue
-			walsFeatLangCount[feat] += 1
-			walsCompList[feat].append( compValue )
-
-		else:
-			walsFeatComp[feat] = compValue
-			walsFeatLangCount[feat] = 1
-			walsCompList[feat] = [ compValue ]
-
-#print walsCompList
-
-
-# Get average complexity
-for feat in walsFeatComp:
-	compval = walsFeatComp[feat]
-	compavg = compval / float(walsFeatLangCount[feat])
-	print "WALS: ", feat, compavg
-
-
-
-cur.close()
-conn.close()
-
-## TO SELF: SEGMENTAL FEATURES?
-## TO SELF: Prohibitive is paradigmatic and syntagmatic complexity? Can I even really test syntagmatic here?
